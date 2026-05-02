4920d1de1dc2b9da69de210ec57c7a00fec4d8bb
Doc1998 <docmp98@gmail.com>
2026-05-02 11:41:55 +0100

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "MossPro",
+    "email": "info@mosspro.ie",
+    "phoneForTel": "0874214667",
+    "phoneFormatted": "(085) 421 4667",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Kildare",
+        "state": "",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/profile.php?id=61587734237446",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.mosspro.ie"
+};
